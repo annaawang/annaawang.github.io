aef6b3abe91f5ff99deb35f3bfea054490b67ac5
annaawang <anna.a.wang@gmail.com>
2016-08-09 22:02:05 -0500

    edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
 
 gulp.task('scripts', function() {
     return gulp.src('process/js/*.js')
@@ -18,8 +20,6 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('builds/js'));
 });
 
-gulp.task('default', ['scripts']);
-
 
 gulp.task('sass', function() {
     return sass('process/sass/styles.scss', {style: 'compressed'})
@@ -27,4 +27,20 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('builds/css'));
 });
 
-gulp.task('default', ['scripts', 'sass']);
+
+gulp.task('images', function() {
+  return gulp.src('process/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('builds/img'));
+});
+
+gulp.task('watch', function() {
+   // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+   // Watch .scss files
+  gulp.watch('src/scss/*.scss', ['sass']);
+   // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+ });
+
+gulp.task('default', ['scripts', 'sass', 'images', 'watch']);
