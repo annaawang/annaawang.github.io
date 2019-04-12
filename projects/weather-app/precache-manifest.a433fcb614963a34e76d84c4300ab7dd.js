6f7f5c0cd0fe313c5d32da10acdbdd2d4dab07ba
annaawang <anna.a.wang@gmail.com>
2019-04-12 18:16:22 -0500

    added weather app

---
+++ projects/weather-app/precache-manifest.a433fcb614963a34e76d84c4300ab7dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c202f5d977b2bd95acc7cd3ff5f32db",
+    "url": "/static/media/clear-day.6c202f5d.svg"
+  },
+  {
+    "revision": "91c2d1e3f6b416905ea7",
+    "url": "/static/css/main.23e7e7f5.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf17c4d59262e482316e",
+    "url": "/static/js/2.5b363b31.chunk.js"
+  },
+  {
+    "revision": "9b39372416fcb9bcf4d1723ce6aaadf3",
+    "url": "/static/media/rain.9b393724.svg"
+  },
+  {
+    "revision": "7b077fa11309bcbac005cf92b6dbfd5b",
+    "url": "/static/media/partly-cloudy-day.7b077fa1.svg"
+  },
+  {
+    "revision": "91c2d1e3f6b416905ea7",
+    "url": "/static/js/main.bedb3d5a.chunk.js"
+  },
+  {
+    "revision": "e32e446d885dd69c7177063a04f78286",
+    "url": "/static/media/cloud.e32e446d.svg"
+  },
+  {
+    "revision": "aebb91ed8b65700af251632ca650c982",
+    "url": "/static/media/location-on-material.aebb91ed.svg"
+  },
+  {
+    "revision": "6dd7ae061b3ec807446d9fbcb50ba87d",
+    "url": "/static/media/dallas@3x.6dd7ae06.png"
+  },
+  {
+    "revision": "ab3021f6ecd01823af6929a0256119fd",
+    "url": "/static/media/thunderstorm.ab3021f6.svg"
+  },
+  {
+    "revision": "949aa8c9576f361d75024d19d6f93809",
+    "url": "/index.html"
+  }
+];
